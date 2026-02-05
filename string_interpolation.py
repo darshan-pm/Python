57901724c2295a9e9943355b6be638a08fc578c8
Darshan PM <darshan200713@gmail.com>
2026-02-06 01:20:01 +0530

    Add files via upload

---
+++ string_interpolation.py
@@ -0,0 +1,8 @@
+name = "Darshan"
+age = 25
+goal = "Ai Engineer"
+collage = "Jain university"
+print(
+    f"my name is {name} I am {age} year old i am studying in {collage} my goal is become an {goal}"
+    "."
+)
