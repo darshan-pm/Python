e360f86eebf8e80a6ca13eda772373a0d7217a81
Darshan PM <darshan200713@gmail.com>
2026-02-06 01:10:55 +0530

    Add files via upload

---
+++ string_concatenation.py
@@ -0,0 +1,16 @@
+name = "Darshan"
+age = 25
+collage = "Jain University"
+goal = "Ai Engineer"
+
+print(
+    "my name is  "
+    + name
+    + " I am "
+    + str(age)
+    + " year old  "
+    + "i am studying in  "
+    + collage
+    + "  my goal is to become an  "
+    + goal
+)
