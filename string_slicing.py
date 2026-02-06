57fb2432e9cafc78948cf4b42e753d93e28bc289
Darshan PM <darshan200713@gmail.com>
2026-02-07 02:18:49 +0530

    Add files via upload

---
+++ string_slicing.py
@@ -0,0 +1,19 @@
+# Question no. 1: Write a Python program to extract the first 5 characters from a given string using string slicing.
+text = "Hello world"
+print(text[:5])
+
+# Question no.2: write a python program to extract the last 4 characters of a string using negative indexing.
+text = "Hello world"
+print(text[-4:])
+
+# Question no.3 Write a python program to print evry second charcter of a string using slicing.
+text = "Hello world"
+print(text[0:10:2])
+
+# Question no.4 Write a Python program to reverse a string using string slicing.
+text = "Hello word"
+print(text[::-1])
+
+# Question no.5 Given an email ID "user@gmail.com", write a Python program to extract the domain name using string slicing.
+text = "user@gmail.com"
+print(text[5:])
