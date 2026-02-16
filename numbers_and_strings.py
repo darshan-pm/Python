474b626eacb3777d4838e0c8ec6fc62933c6ca02
Darshan PM <darshan200713@gmail.com>
2026-02-17 01:58:45 +0530

    Add files via upload

---
+++ numbers_and_strings.py
@@ -0,0 +1,15 @@
+# Write a program that takes a number and prints its square and cube.
+# square
+square_of_number = 3
+square_of_number **= 2
+print(square_of_number)
+
+# cube
+cube_of_number = 4
+cube_of_number **= 3
+print(cube_of_number)
+
+# string
+name = "Darshan"
+name *= 3
+print(name)
