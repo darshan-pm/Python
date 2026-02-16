d6308de5b301e069e42da6c19cc6cffae16e3f50
Darshan PM <darshan200713@gmail.com>
2026-02-17 01:10:52 +0530

    Add files via upload

---
+++ numbers_and_math_operations.py
@@ -0,0 +1,30 @@
+# Write a program that takes two numbers as input and performs basic arithmetic operations (addition, subtraction, multiplication, division,floor division,modulus) on them.
+# 1.Addition:
+sum_of_number = 8
+sum_of_number += 2
+print(sum_of_number)
+
+# 2.subtraction:
+difference_of_number = 10
+difference_of_number -= 2
+print(difference_of_number)
+
+# 3.Multiplication:
+product_of_number = 3
+product_of_number *= 4
+print(product_of_number)
+
+# 4.Division:
+division_of_number = 4
+division_of_number /= 2
+print(division_of_number)
+
+# 5.floor division:
+floor_division_of_number = 24
+floor_division_of_number //= 5
+print(floor_division_of_number)
+
+# 6.modulus:
+counter_of_number = 26
+counter_of_number %= 5
+print(counter_of_number)
